5546d670db70d76e1991bad66d58195958aeb457
Nikolay <cave222@list.ru>
2020-10-16 16:23:39 +0300

    Nice one

---
+++ step_1/js.js
@@ -0,0 +1,32 @@
+//variable
+let name = "vasia";
+let b = 'Hello';
+let c = `Hello`;
+let number = 6;
+
+console.log("Hello "+name+", you are the best!");
+console.log('number = $(number)');
+//code into new line
+console.log("Hello , you are the best!Hello , you are the best!Hello , you are the best!\
+Hello , you are the best!");
+console.log("Hello \'quote'\ u are the best!!!!")
+
+//next type of variable is number
+const a = 1;
+const pi = 3.14;
+
+console.log(a);
+console.log(pi);
+
+let str = "8";
+console.log(typeof str);
+let num = 7;
+//convert str to number 
+let num2 = +str;
+let res = num2+7;
+console.log(typeof res)
+console.log(res)
+console.log(typeof a)
+
+let num4 = 0
+console.log(10/-num4)
