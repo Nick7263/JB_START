23d5d9613213507a59bcd6d09a583862ef42383c
Nikolay <cave222@list.ru>
2020-10-19 16:21:28 +0300

    123

--- step_1/js.js
+++ step_1/js.js
@@ -1,32 +1,69 @@
 //variable
-let name = "vasia";
-let b = 'Hello';
-let c = `Hello`;
-let number = 6;
-
-console.log("Hello "+name+", you are the best!");
-console.log('number = $(number)');
-//code into new line
-console.log("Hello , you are the best!Hello , you are the best!Hello , you are the best!\
-Hello , you are the best!");
-console.log("Hello \'quote'\ u are the best!!!!")
-
-//next type of variable is number
-const a = 1;
-const pi = 3.14;
-
-console.log(a);
-console.log(pi);
-
-let str = "8";
-console.log(typeof str);
-let num = 7;
-//convert str to number 
-let num2 = +str;
-let res = num2+7;
-console.log(typeof res)
-console.log(res)
-console.log(typeof a)
-
-let num4 = 0
-console.log(10/-num4)
+// let name = "vasia";
+// let b = 'Hello';
+// let c = `Hello`;
+// let number = 6;
+// console.log("Hello "+name+", you are the best!");
+// console.log('number = $(number)');
+// //code into new line
+// console.log("Hello , you are the best!Hello , you are the best!Hello , you are the best!\
+// Hello , you are the best!");
+// console.log("Hello \'quote'\ u are the best!!!!")
+// //next type of variable is number
+// const a = 1;
+// const pi = 3.14;
+// console.log(a);
+// console.log(pi);
+// let str = "8";
+// console.log(typeof str);
+// let num = 7;
+// //convert str to number 
+// let num2 = +str;
+// let res = num2+7;
+// console.log(typeof res)
+// console.log(res)
+// console.log(typeof a)
+// let num4 = 0
+// console.log(10/-num4)
+// console.log(`number = ${number}`);
+//Step_1 less_2
+// let a = 5;
+// console.log(typeof a);
+// a = 7;
+// console.log(typeof a);
+// a = "Hello"
+// console.log(typeof a);
+// console.log(typeof a);
+//break point
+// tr to number
+let a = b = "3.14";
+a = parseInt("3.14");
+b = parseFloat("3.14")
+console.log(`Pi equals ${a},Pi after float ${b}`);
+a = parseInt("3px");
+b = parseFloat("3.14px")
+console.log(typeof b)
+console.log(`Pi equals ${a},Pi after float ${b}`);
+/* data types
+string
+number
+boolean
+const a = true;
+const b = false;
+const a = null;
+console = undefined;
+null
+//undefined
+let _new;;
+console.log(typeof _new);
+*/
+const myObject = {
+    myKey : "Hello",
+    age :120
+}
+let Greetings = myObject.myKey;
+console.log(Greetings);
+let number = myObject['age'];
+console.log(number);
+let anyAnswer = prompt();
+console.log(anyAnswer);
