345a52771abbba943f74785ef7ab6f6262b7862d
Nikolay <cave222@list.ru>
2020-10-26 17:33:39 +0300

    starting to make a shop

---
+++ step_1/second.js
@@ -0,0 +1,9 @@
+let admin,name;
+name = prompt("Введите имя");
+admin = "Петя";
+if(name == admin){
+    document.writeln(`Hello ${admin}`)
+}
+else{
+    document.writeln(`Hello ma friend ${admin}`)
+}
---
+++ step_2/app.js
@@ -0,0 +1,17 @@
+let goodsCount = 0;
+    
+if(goodsCount>0){
+    console.log(`The amount of items ${goodsCount}`);
+}
+else{
+    console.log('The bin is empty.')
+}
+// if(goodsCount === 0){
+    // console.log(`The bin is empty`);
+// }
+// if(goodsCount = 1723){
+    // console.log(`The bin is empty2`);
+// }
+// if(goodsCount == false){
+    // console.log(`The bin is empty3`);
+// }
