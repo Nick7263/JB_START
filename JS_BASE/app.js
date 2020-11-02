5ac7ec3a1a5e55991c6e0526f5fc0a84d5177a37
Nick <cave222@list.ru>
2020-11-02 18:01:37 +0200

    new stuff

--- JS_BASE/app.js
+++ JS_BASE/app.js
@@ -1,1 +1,20 @@
-let express = require('express')
+//Подключаем установленный модуль через команду require.
+let express = require('express');
+//создаём новый объект как экхемпляр класса express
+let app = express();
+
+let port = 3000;
+
+app.listen(port,function(){
+console.log('node express work on 3000')
+});
+
+//Далее чтобы запустить первое приложение
+//get метод обращения 
+app.get('/',function(req,res){
+    res.end('<h1>Hello</h1>Hello my first super landing page');
+});
+
+app.get('/cat',function(req,res){
+    res.end('<h1>Category</h1>');
+});
